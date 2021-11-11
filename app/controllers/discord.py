4a4044c181771d49020eb7c24bbd27ce60739213
Leonardo Marcão <leonardo.marcao@unesp.br>
2021-11-11 18:34:41 -0300

    Test

--- app/controllers/discord.py
+++ app/controllers/discord.py
@@ -1,8 +1,15 @@
 from flask_restful import Resource
+from flask import request
 
 
-class Discord(Resource):    
-    def get(self):
+class DiscordPullRequest(Resource):    
+    def post(self):
         return {
-            "message": "get: hello from the testcontroller"
+            "data": request.data
+        }
+
+class DiscordDeployCompleted(Resource):
+    def post(self):
+        return {
+            "data": request.data
         }
--- app/main/api.py
+++ app/main/api.py
@@ -1,5 +1,5 @@
 from flask_restful import Api
-from app.controllers.discord import Discord
+from app.controllers.discord import DiscordDeployCompleted, DiscordPullRequest
 from app.main.errors import errors
 
 # Flask API Configuration
@@ -9,6 +9,7 @@ api = Api(
     prefix='/api'
 )
 
-api.add_resource(Discord, '/discord')
+api.add_resource(DiscordPullRequest, '/discord_pr_notification')
+api.add_resource(DiscordDeployCompleted, '/discord_deploy_notification')
 
 
