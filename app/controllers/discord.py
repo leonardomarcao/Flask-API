642f767fcba46bea3dc39dee88555b5af7397114
Leonardo Marcão <leonardo.marcao@unesp.br>
2021-11-11 18:37:52 -0300

    Adds jsonify

--- app/controllers/discord.py
+++ app/controllers/discord.py
@@ -1,15 +1,15 @@
 from flask_restful import Resource
-from flask import request
+from flask import request, jsonify
 
 
 class DiscordPullRequest(Resource):    
     def post(self):
-        return {
+        return jsonify({
             "data": request.data
-        }
+        })
 
 class DiscordDeployCompleted(Resource):
     def post(self):
-        return {
+        return jsonify({
             "data": request.data
-        }
+        })
