1f84faf3b60cc24e19895778566d3e7dd0b67214
Leonardo Marcão <leonardo.marcao@unesp.br>
2021-11-11 18:09:49 -0300

    Updates for test

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from app import main
 from app.main.api import api
 
@@ -11,24 +11,9 @@ app.config.from_object(main.settings[os.environ.get('APPLICATION_ENV', 'default'
 # Logs Initialization
 console = logging.getLogger('console')
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/pr-azure', methods=['POST'])
+def get_pr_from_azure():
+    return jsonify({'pr_body': request.form})
 
 
 # Flask API Initialization
